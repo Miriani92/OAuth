4494356c8893077d07f300e4673819dd55fb79a9
Miriani92 <mac@Miriani.local>
2023-12-28 15:27:24 +0400

    add buttons

--- client/src/pages/Registration.tsx
+++ client/src/pages/Registration.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Container } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
+import GithubIcon from "@mui/icons-material/GitHub";
+import { Container, Button, Box, Typography } from "@mui/material";
 import { NavigationBar } from "../components";
+import { green, red } from "@mui/material/colors";
 
 export const Registration = () => {
   return (
@@ -9,9 +12,39 @@ export const Registration = () => {
       sx={{
         display: "flex",
         alignItems: "center",
+        flexDirection: "column",
       }}
     >
       <NavigationBar />
+      <Box
+        sx={{
+          p: 2,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 1,
+          height: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h4">Sign with Providers</Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          fullWidth
+          startIcon={<GoogleIcon />}
+        >
+          Sign in with google
+        </Button>
+        <Button
+          variant="contained"
+          startIcon={<GithubIcon />}
+          color="success"
+          fullWidth
+        >
+          Sign in with github
+        </Button>
+      </Box>
     </Container>
   );
 };
