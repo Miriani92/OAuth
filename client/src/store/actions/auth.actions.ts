48c2422637c67bdc9eba710fa836c4a9d92bd9d0
Miriani92 <mac@Miriani.local>
2024-01-01 21:22:06 +0400

    fix cors bug

--- client/src/store/actions/auth.actions.ts
+++ client/src/store/actions/auth.actions.ts
@@ -6,9 +6,10 @@ export const getCurrentUser = createAsyncThunk<User>(
   "data/getCurrentUser",
   async () => {
     try {
-      const response = await fetch(
-        "http://localhost:4000/api/v1/auth/sessions/show_me"
-      );
+      const response = await fetch(`${BASE_URL}${endPoints.SHOW_ME}`, {
+        method: "GET",
+        credentials: "include",
+      });
       const data = await response.json();
       // TO DO ---> types: response
       return { user: data?.user } as any;
--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import logger from "./utils/logger";
 import env from "dotenv";
@@ -13,8 +14,14 @@ const port = process.env.PORT || 4000;
 const app: Application = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/auth", authRouter);
 
